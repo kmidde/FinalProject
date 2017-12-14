bb5bc6be49896e904789118e1c5e9e1009966c67
bhackel <braishaekl@gmail.com>
2017-12-14 09:41:06 -0800

    stuff

--- _JS/rpg.js
+++ _JS/rpg.js
@@ -5,11 +5,14 @@
 var player = { //player object
 	name:"",
 	godMode:false,
+	allowMove: false,
+	display:false,
 	x:10,
 	y:360,
 	walkingSpeed:1,
 	movementSpeed:3,
 	runningSpeed:10,
+	speed:3,
 };
 
 var game = { //game object
@@ -18,7 +21,7 @@ var game = { //game object
 
 console.log("screen size of "+window.innerWidth+"x"+window.innerHeight);
 
-var frameRun;var keyRun;
+var frameRun;
 
 function start() {
 	"use strict";
@@ -34,16 +37,21 @@ function start() {
 		document.getElementById("player").setAttribute('src', '../_images/God.png');
 		player.godMode = true;
 	}
-	frameRun = setInterval(frame, 10);//runs frame every 0.01 second
 	
+	document.getElementById('content').innerHTML += '<button id="" onclick="china()">China</button><button id="" onclick="middle()">Middle Ages</button><button id="" onclick="americas()">Americas</button';
+	frameRun = setInterval(frame, 10);//runs frame every 0.01 second
 }
 
 function frame() {
 	if (game.run === true) {
 		checkKey();
+		document.getElementById("playerImage").style = 'position: absolute;left:'+(player.x-25)+'px;top:+'+(player.y-25)+'px;';
 		if (player.y <= 0 || player.y >= 720 || player.x <= 0 || player.x >= 1280) { //checking if player has left the canvas, then kills them
 			game.run = false;
 		}
+		if (player.display === false) {
+			document.getElementById("playerImage").style.display = 'none';
+		} else { document.getElementById("playerImage").style.display = 'inherit';}
 		
 	}
 	else { //displays death message when game ends
@@ -69,7 +77,8 @@ window.onkeydown = function(e){
      if(kc === 38) {Keys.up = true;}
      if(kc === 39) {Keys.right = true;}
      if(kc === 40) {Keys.down = true;}
-	 if(kc === 16) {player.movementSpeed = player.maxMovementSpeed;}
+	 if(kc === 16) {player.speed = player.runningSpeed;}
+	 if(kc === 18) {player.speed = player.walkingSpeed;}
  };
 
 window.onkeyup = function(e){
@@ -80,24 +89,55 @@ window.onkeyup = function(e){
      if(kc === 38) {Keys.up = false;}
      if(kc === 39) {Keys.right = false;}
      if(kc === 40) {Keys.down = false;}
-	 if(kc === 16) {player.movementSpeed = player.minMovementSpeed;}
+	 if(kc === 16) {player.speed = player.movementSpeed;}
+	 if(kc === 18) {player.speed = player.movementSpeed;}
 };
 
 function checkKey(){
-    if(Keys.up){
-        player.y -= player.movementSpeed;
-    }
+	if (player.allowMove === true) {
+		if(Keys.up){
+			player.y -= player.speed;
+		}
 
-    if(Keys.down){
-        player.y += player.movementSpeed;
-    }
+		if(Keys.down){
+			player.y += player.speed;
+		}
 
-    if(Keys.left) {
-        player.x -= player.movementSpeed;
-    }
+		if(Keys.left) {
+			player.x -= player.speed;
+		}
 
-    if(Keys.right){
-        player.x += player.movementSpeed;
-    }
-	document.getElementById("playerImage").style = 'position: absolute;left:'+(player.x-25)+'px;top:+'+(player.y-25)+'px;';
+		if(Keys.right){
+			player.x += player.speed;
+		}
+	}
+	
+}
+
+function checkInteract(x1,y1,x2,y2,w1,h1,w2,h2){
+	if (x1 < x2+w2 && x1+w1 > x2-w2 && y1 < y2+h2 && y1+h1 > y2-h2) {
+		return true;
+	}
+}
+
+
+//moveAnimate((boss.x+250),(boss.y+200),(tempx),(tempy),'kitten',25,25);
+var pos = 0;
+var maxpos = 200;
+function moveAnimate(x1,y1,x2,y2,elem,xoff,yoff,intID) {
+	if (pos === maxpos) {
+		document.getElementById(elem).style = 'position: absolute;display: none;';
+		pos = 0;
+		clearInterval(intID);
+	} else {
+		pos++;
+		document.getElementById(elem).style = 'position: absolute;display: inherit;left: '+(Math.floor(x1+(pos*((x2-x1)*1.0/maxpos)))-xoff)+'px;top: '+(Math.floor(y1+(pos*((y2-y1)*1.0/maxpos)))-yoff)+'px;';//calculates distance to change per tick lol what a great explanation
+	}
+}
+
+
+//games
+
+function china() {
+	document.getElementById("content").innerHTML = '';
 }
